54f11d2fa84e58d7c5fc448c793f6ec81d2e2ba5
vicarb <vcardenasb@protonmail.com>
2023-03-29 16:51:32 -0300

    Post Component fix

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -9,9 +9,9 @@ type PostProps = {
 
 const Post: React.FC<PostProps> = ({ title, body, author, date }) => {
   return (
-    <div className="p-6 bg-white rounded-lg shadow-md text-center">
+    <div className="p-6 bg-white rounded-lg shadow-md md:text-center">
       <h2 className="text-4xl font-bold mb-4 sm:mt-10">{title}</h2>
-      <p className="text-gray-700 text-2xl mb-4 mx-40">{body}</p>
+      <p className="text-gray-700 text-2xl mb-4 lg:mx-40">{body}</p>
       <div className="flex items-center justify-between">
         <p className="text-gray-600 text-lg">{author}</p>
         <p className="text-gray-600 text-lg">{date}</p>
