183dda04e68c77df57ec70c5e1043b2f6dd379b6
vicarb <vcardenasb@protonmail.com>
2023-03-28 21:51:29 -0300

    Text & Image Component v1

--- components/TextImage/TextImage.tsx
+++ components/TextImage/TextImage.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image';
 
 const TextImage = () => {
   return (
-    <div className="flex flex-row h-screen">
-      <div className="flex-1 flex justify-center items-center p-10">
+    <div className="flex flex-col h-screen md:flex-row">
+      <div className="md:flex-1 flex justify-center items-center p-10">
         <h1 className="text-4xl font-bold text-center">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit
         </h1>
       </div>
-      <div className="flex-1 flex justify-center items-center">
-        <Image alt="369"src="https://picsum.photos/800/600?random=6" width={1200} height={800} objectFit="cover" />
+      <div className="md:flex-1 flex justify-center items-center">
+        <Image alt="12312" src="https://picsum.photos/800/600?random=6" width={1200} height={800} objectFit="cover" />
       </div>
     </div>
   )
