42e8ffdad6320766c4a5f4a878d85ba0c5db3666
vicarb <108682674+vicarb@users.noreply.github.com>
2023-11-21 20:57:41 -0300

    Update ChildrenLayout.tsx

--- components/ChildrenLayout/ChildrenLayout.tsx
+++ components/ChildrenLayout/ChildrenLayout.tsx
@@ -12,7 +12,7 @@ const ChildrenLayout: React.FC<ChildrenLayoutProps> = ({
   return (
     <div className="flex flex-col items-center justify-center bg-gray-100 p-6 rounded-lg shadow-md">
       <h2 className="text-4xl font-bold text-gray-800 text-center py-4">{header}</h2>
-      <p className="text-xl text-gray-500 text-center mt-2 px-4">{subheader}</p>
+      <p className="text-xl text-gray-500 text-center mt-2 px-6">{subheader}</p>
     </div>
   );
 };
